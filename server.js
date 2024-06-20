fcee913d1c780ed2dc2c631c0f4e48a7a534c069
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2024-06-20 16:13:31 +0530

    Changes to server.js

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ require('dotenv').config()
 const app = express();
 
 const PORT = process.env.PORT;
-const HOSTNAME = process.env.HOSTNAME;
 
 app.get('/', (req, res)=>{
     res.status(200).send('<h1>Deploying a Node JS Application on EC2</h1>');
 })
 
-app.listen(PORT,HOSTNAME, ()=>{
-    console.log(`Server listening at http://${HOSTNAME}:${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`Server listening at ${PORT}`);
 })
